fbea5688abefa7cf9b72bba4061c2834084ecba0
APE-LAIR\Admin <andie.ape@gmail.com>
2020-05-18 04:32:51 +0300

    ss

---
+++ katanaParts.js
@@ -0,0 +1,94 @@
+const colorsNonMetal = [
+    {
+        color: 'DE300B'
+    },
+    {
+        color: '6CA11F'
+    },
+    {
+        color: '1FA17C'
+    },
+    {
+        color: '1CE3DA'
+    },
+    {
+        color: '1CA4E3'
+    },
+    {
+        color: 'E6E613'
+    },
+    {
+        color: '8313E6'
+    },
+    {
+        color: 'C913E6'
+    },
+    {
+        color: 'E61313'
+    },
+    {
+        color: '626262'
+    },
+    {
+        color: '2ADF11'
+    },
+    ]
+
+    const colorsMetal = [
+        {
+            color: '000000'
+        },
+        {
+            color: 'CD7F32'
+        },
+        {
+            color: 'C0C0C0'
+        },
+        {
+            color: 'D4AF37'
+        },
+
+    ]
+
+    const parts = [
+
+        {
+            part: 'handle_low_Katana_0002',
+            name: 'Цукаито',
+            metal: false
+        },    
+        {
+            part: 'buttcap_low_Katana_0002',
+            name: 'Кашира',
+            metal: true
+        },
+        {
+            part: 'handguard_low_Katana_0002',
+            name: 'Цуба',
+            metal: true
+        },
+            
+        {
+            part: 'metal_sleve_low_Katana_0002',
+            name: 'Фути',
+            metal: true
+        },
+        
+        {
+            part: 'sageo_wrap_low_Scabbard_0002',
+            name: 'Сагео',
+            metal: false
+        },
+        {
+            part: 'scabbard_low_Scabbard_0002',
+            name: 'Коигучи',
+            metal: true
+        },
+        {
+            part: 'scabbard_sleve_low_Katana_0002',
+            name: 'Хабаки',
+            metal: true
+        }
+
+        
+    ]   
---
+++ main.js
@@ -0,0 +1,261 @@
+var loaded = false;
+const LOADER = document.getElementById('js-loader');
+const DRAG_NOTICE = document.getElementById('js-drag-notice');
+
+let scene;
+const canvas = document.querySelector('#c');
+const BACKGROUND_COLOR = 0x4f4f4f;
+
+var TRAY;
+const PICKER = document.getElementById('pick-part');
+
+var colors = colorsNonMetal;
+
+
+function init() {
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(BACKGROUND_COLOR);
+
+    camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight, 1, 5000);
+    
+    camera.rotation.y = 45/180*Math.PI;
+    camera.position.x = 0;
+    camera.position.y = 0;
+    camera.position.z = 500;
+
+    
+    
+    
+    var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+    scene.add( light );
+
+    var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 5);
+    hemiLight.position.set( 0, 10, 0 );
+
+    scene.add( hemiLight );
+
+
+    light1 = new THREE.PointLight(0xc4c4c4,1);
+    light1.position.set(30,20,50);
+    scene.add(light1);
+
+    light2 = new THREE.PointLight(0xc4c4c4,1);
+    light2.position.set(-7,20,50);
+    scene.add(light2);
+
+    light3 = new THREE.PointLight(0xc4c4c4,1);
+    light3.position.set(30,-20,-50);
+    scene.add(light3);
+    
+    light4 = new THREE.PointLight(0xc4c4c4,1);
+    light4.position.set(-7,20,-50);
+    scene.add(light4);
+
+   
+
+
+    lightFront = new THREE.DirectionalLight(0xc4c4c4, 1);
+    lightFront.position.set(0,12, 10);
+    scene.add(lightFront);
+
+    lightBack = new THREE.DirectionalLight(0xc4c4c4, 1);
+    lightBack.position.set(0,12, -10);
+    scene.add(lightBack);
+
+    
+   
+
+    renderer = new THREE.WebGLRenderer({canvas, antialias:true});
+
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+    controls.addEventListener('change', renderer);
+
+//controls.update() must be called after any manual changes to the camera's transform
+    camera.position.set( 0, 0, 50 );
+
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+
+    let loader = new THREE.GLTFLoader();
+    loader.load('models/katana/katana/katana-rr.gltf', function(obj) {
+        katana = obj.scene.children[0];
+        katana.scale.set(1, 1, 1);
+        
+        kataParts = katana.children[0].children[0].children[0];       
+     
+
+       scene.add(katana);
+       LOADER.remove();
+       animate();
+       
+       
+    }, undefined, function(error) {
+        console.error(error)
+      }
+    );
+    
+}
+
+function animate() {
+
+ 
+  renderer.render(scene,camera);
+  requestAnimationFrame(animate);
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+  if (katana != null && loaded == false) {
+    
+    DRAG_NOTICE.classList.add('start');
+  }
+  
+  
+
+  
+}
+
+init();
+
+
+PICKER.addEventListener('change', function() {
+    var metalCheck = PICKER[this.selectedIndex].getAttribute('metal');
+    var metalCols = document.getElementById('slide_metal');
+    var nonmetalCols = document.getElementById('slide_nonmetal');
+
+    console.log(metalCheck)
+    
+    if (metalCheck === "false"){        
+        metalCols.classList.add('hidden');
+        nonmetalCols.classList.remove('hidden');
+
+        colors = colorsNonMetal;
+        
+    } else {
+        metalCols.classList.remove('hidden');
+        nonmetalCols.classList.add('hidden');
+        colors = colorsMetal;
+    }  
+      
+    
+})
+
+
+
+function buildColors(colors) {
+    console.log(colors.length);
+    for (let [i, color] of colors.entries()) {
+      let swatch = document.createElement('div');
+      swatch.classList.add('tray__swatch');
+    if (colors.length < 6 ){
+       TRAY = document.getElementById('slide_metal');
+    }else {
+       TRAY = document.getElementById('slide_nonmetal');
+    }
+            
+      if (color.texture)
+      {
+        swatch.style.backgroundImage = "url(" + color.texture + ")";   
+      } else
+      {
+        swatch.style.background = "#" + color.color;
+      }
+  
+      swatch.setAttribute('data-key', i);
+      TRAY.append(swatch);
+    }
+
+    
+  }
+  
+  
+  buildColors(colorsNonMetal);
+  buildColors(colorsMetal);
+
+
+
+function buildPartPicker(parts){
+    for (let part of parts){
+        var option = document.createElement('option');
+        option.setAttribute('value', part.part);
+        option.innerHTML = part.name;
+        option.setAttribute('metal', part.metal)
+        PICKER.append(option);
+     }
+    }
+    buildPartPicker(parts);
+
+  const swatches = document.querySelectorAll(".tray__swatch");
+
+for (const swatch of swatches) {
+  swatch.addEventListener('click', selectSwatch);
+}
+
+function selectSwatch(e) {
+    let color = colors[parseInt(e.target.dataset.key)];
+   
+
+   var new_clr = parseInt('0x' + color.color);   
+   setMaterial(katana, PICKER.value, new_clr);
+}
+
+function setMaterial(parent, type, new_clr) {
+    var also = null;
+    
+    parent.traverse((o) => {
+        
+      
+     if (o instanceof THREE.Mesh && o.name != null) {
+     
+       if (o.name == type) {
+           console.log(type)
+           if (type == 'handguard_low_Katana_0002'){
+            var also = scene.getObjectByName("metal_spacer_btm_low_Katana_0002"); 
+            console.log(also);
+           } else if (type == 'scabbard_sleve_low_Katana_0002'){
+            var also = scene.getObjectByName("metal_spacer_up_low_Katana_0002"); 
+           } else if (type == 'handle_low_Katana_0002'){
+            var also = scene.getObjectByName("handle_wrap_end_low_Katana_0002"); 
+           }
+
+
+            var mtl = o.material.clone();
+            mtl.color.setHex(new_clr);
+            mtl.metalness = 1;
+           
+            mtl.blending = THREE.CustomBlending;
+            o.material = mtl;
+            if (also){
+                also.material = mtl;
+            }
+            
+       
+        }
+     }
+   });
+  }
+
+
+  let initRotate = 0;
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var canvasPixelWidth = canvas.width / window.devicePixelRatio;
+    var canvasPixelHeight = canvas.height / window.devicePixelRatio;
+  
+    const needResize = canvasPixelWidth !== width || canvasPixelHeight !== height;
+    if (needResize) {
+      
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+
+ 
